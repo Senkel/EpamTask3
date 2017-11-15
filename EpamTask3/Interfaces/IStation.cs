fa1cccf80c99263afcb6d52bf129ca83d241c100
Senkel <robfutbol@mail.ru>
2017-11-15 16:04:50 +0300

    Interfaces

---
+++ EpamTask3/Interfaces/IStation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTask3.Interfaces
+{
+    interface IStation:IClearEventHandlers
+    {
+        void RegisterEventHandlersForTerminal(ITerminal terminal);
+        void RegisterEventHandlersForPort(IPort port);
+    }
+}
---
+++ EpamTask3/Interfaces/ITerminal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTask3.Interfaces
+{
+    interface ITerminal:IClearEventHandlers
+    {
+        event EventHandler Online; // when terminal is going to call mode
+
+        event EventHandler Offline; // when the connection is interrupted
+
+        event EventHandler Plugging; // when user plug the device
+
+        event EventHandler UnPlugging; // when user unplug the device
+
+        void Drop();
+
+        void Answer();
+
+        void Plug();
+
+        void Unplug();
+
+        void RegisterEventHandlersForPort(IPort port);
+    }
+}
